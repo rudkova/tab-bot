4a2867c837833c0e1ddfec36907664d255e60421
Lucy Rudkova <lucy.rudkova@gmail.com>
2025-07-13 17:16:58 +0300

    add stub commands

--- src/bot/router.ts
+++ src/bot/router.ts
@@ -5,6 +5,14 @@ import { Telegraf } from 'telegraf';
  * @param bot The Telegraf bot instance
  */
 export function setupCommands(bot: Telegraf) {
+  // Set up the bot commands menu
+  bot.telegram.setMyCommands([
+    { command: 'start', description: 'Start the bot' },
+    { command: 'help', description: 'Show help message' },
+    { command: 'add_medication', description: 'Add a new medication' },
+    { command: 'contact_developer', description: 'Contact the developer' },
+  ]);
+
   bot.start(ctx => {
     return ctx.reply('Welcome to Tab-Bot! I can help you track your medicine expiration dates.');
   });
@@ -14,7 +22,23 @@ export function setupCommands(bot: Telegraf) {
       'I can help you track your medicine expiration dates.\n\n' +
         'Available commands:\n' +
         '/start - Start the bot\n' +
-        '/help - Show this help message'
+        '/help - Show this help message\n' +
+        '/add_medication - Add a new medication\n' +
+        '/contact_developer - Contact the developer'
+    );
+  });
+
+  // Add medication command
+  bot.command('add_medication', ctx => {
+    return ctx.reply(
+      'Please provide details about the medication you want to add (name, expiration date, etc.)'
+    );
+  });
+
+  // Write to developer command
+  bot.command('contact_developer', ctx => {
+    return ctx.reply(
+      'If you have any questions or suggestions, please write them here and they will be forwarded to the developer.'
     );
   });
 
