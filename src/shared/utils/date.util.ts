1f5a722c4ece90777053a94a50a740ffcf6fb1f9
Lucy Rudkova <lucy.rudkova@gmail.com>
2025-07-16 12:47:49 +0300

    implement parseDate util

---
+++ src/shared/utils/date.util.ts
@@ -0,0 +1,31 @@
+import { parse, isValid, format, addMonths, isBefore } from 'date-fns';
+import { config } from '../../configs/config.ts';
+
+const parseDate = (
+  dateStr: string
+): {
+  parsedDate?: Date;
+  errorMessage?: string;
+} => {
+  const parsedDate = parse(dateStr, config.app.dateFormat, new Date());
+
+  if (!isValid(parsedDate) || format(parsedDate, config.app.dateFormat) !== dateStr) {
+    return {
+      errorMessage: `Invalid date format. Please enter the date in ${config.app.dateFormat.toUpperCase()} format:`,
+    };
+  }
+
+  const twoMonthsFromNow = addMonths(new Date(), 2);
+  if (isBefore(parsedDate, twoMonthsFromNow)) {
+    return {
+      errorMessage:
+        'Date must be at least two months from today. Please enter the date in ${config.app.dateFormat.toUpperCase()} format:',
+    };
+  }
+
+  return {
+    parsedDate,
+  };
+};
+
+export { parseDate };
