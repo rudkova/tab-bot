91b2b29710b40f7fb754e69617364c33803d6bf6
Lucy Rudkova <lucy.rudkova@gmail.com>
2025-07-31 16:19:33 +0300

    create winston logger

---
+++ src/shared/logger/logger.ts
@@ -0,0 +1,39 @@
+import winston, { createLogger, format } from 'winston';
+import { config } from '../../configs/config.ts';
+import TransportStream from 'winston-transport';
+import path from 'path';
+
+const isProduction = config.app.env === 'prod';
+const transports: TransportStream[] = [];
+
+if (!isProduction) {
+  transports.push(
+    new winston.transports.Console({
+      format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
+    })
+  );
+}
+
+if (isProduction) {
+  transports.push(
+    new winston.transports.File({
+      filename: path.join('logs', 'app.log'),
+      maxsize: 5242880, // 5MB
+      maxFiles: 5,
+    }),
+    new winston.transports.File({
+      filename: path.join('logs', 'error.log'),
+      level: 'error',
+      maxsize: 5242880,
+      maxFiles: 5,
+    })
+  );
+}
+
+const logger = createLogger({
+  level: 'info',
+  format: format.json(),
+  transports,
+});
+
+export default logger;
