bdfde70e9db94f6ad38eb827a9f620ebde2290c7
Lucy Rudkova <lucy.rudkova@gmail.com>
2025-09-12 11:19:41 +0300

    add rule to remove unused imports on "eslint --fix"

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,29 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
   {
+    plugins: {
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
     ignores: ['dist/**', 'node_modules/**', 'build/**', 'jest.config.js', '.env', '.env.*'],
   }
 );
